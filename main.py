ed469f8e74457be130bff8064a796669cf16c2eb
Yarik-Yashin <59434040+Yarik-Yashin@users.noreply.github.com>
2023-12-03 21:58:06 +0300

    Диаграммы для нескольких графиков

--- main.py
+++ main.py
@@ -52,56 +52,57 @@ class OrderQueue:
 pl = plt.figure()
 camera = Camera(pl)
 limit_order_book = [OrderQueue(i) for i in range(100)]
-a = 0
-b = 100
-while t < 100:
-    m = (a + b) / 2
+middle_prices = [[], [], [], [], [], [], [], [], [], []]
+for r in range(10):
+    a = 0
+    b = 100
+    t = 0
+    while t < 1000:
+        m = (a + b) / 2
 
-    for i in range(max(int(m) - spread * 5, 0), min(int(m) + spread * 5, 100)):
-        la = value_of_poisson(LAMBDA, 10)
-        for j in range(la):
-            v = value_of_poisson(LAMBDA / NU, 10) + 1
-            if i <= m:
-                limit_order_book[i].add_order(Order(v, t))
-                a = i if a <= i else a
-            else:
-                limit_order_book[i].add_order((Order(-v, t)))
-                b = i if b >= i else b
+        for i in range(max(int(m) - spread * 5, 0), min(int(m) + spread * 5, 100)):
+            la = value_of_poisson(LAMBDA, 10)
+            for j in range(la):
+                v = value_of_poisson(LAMBDA / NU, 10) + 1
+                if i <= m:
+                    limit_order_book[i].add_order(Order(v, t))
+                    a = i if a <= i else a
+                else:
+                    limit_order_book[i].add_order((Order(-v, t)))
+                    b = i if b >= i else b
 
-    for i in range(50 - spread * 5, 50 + spread * 5):
-        for_delete = list()
-        for j in range(len(limit_order_book[i].orders)):
-            nu = value_of_poisson(NU, 1)
-            if nu:
-                for_delete.append(j)
-        for j in for_delete[::-1]:
-            limit_order_book[i].cancel_order(j)
-    market_sell = value_of_poisson(MU, 10)
-    market_buy = value_of_poisson(MU, 10)
-    for i in range(b, 100):
-        for j in range(len(limit_order_book[i].orders)):
-            if abs(limit_order_book[i].orders[j].volume) >= market_buy:
-                limit_order_book[i].orders[j].volume += market_buy
-            else:
-                market_buy -= abs(limit_order_book[i].orders[j].volume)
-                limit_order_book[i].orders[j].volume = 0
-    for i in range(a, 0, -1):
-        for j in range(len(limit_order_book[i].orders)):
-            if limit_order_book[i].orders[j].volume >= market_sell:
-                limit_order_book[i].orders[j].volume -= market_sell
-            else:
-                market_sell -= limit_order_book[i].orders[j].volume
-                limit_order_book[i].orders[j].volume = 0
-    for i in range(b, 100):
-        if i == b and limit_order_book[i].order_sum() == 0 and b != 100:
-            b += 1
-    for i in range(a, 0, -1):
-        if i == a and limit_order_book[i].order_sum() == 0 and a != 0:
-            a -= 1
-    t += DT
-    pl = [i.order_sum() for i in limit_order_book]
-    plt.bar([k for k in range(100)], pl, color='b')
-    plt.text(0, 0, f"Mid price = {m}")
-    camera.snap()
-animation = camera.animate()
-animation.save('celluloid_minimal.gif', writer='Pillow')
+        for i in range(50 - spread * 5, 50 + spread * 5):
+            for_delete = list()
+            for j in range(len(limit_order_book[i].orders)):
+                nu = value_of_poisson(NU, 1)
+                if nu:
+                    for_delete.append(j)
+            for j in for_delete[::-1]:
+                limit_order_book[i].cancel_order(j)
+        market_sell = value_of_poisson(MU, 10)
+        market_buy = value_of_poisson(MU, 10)
+        for i in range(b, 100):
+            for j in range(len(limit_order_book[i].orders)):
+                if abs(limit_order_book[i].orders[j].volume) >= market_buy:
+                    limit_order_book[i].orders[j].volume += market_buy
+                else:
+                    market_buy -= abs(limit_order_book[i].orders[j].volume)
+                    limit_order_book[i].orders[j].volume = 0
+        for i in range(a, 0, -1):
+            for j in range(len(limit_order_book[i].orders)):
+                if limit_order_book[i].orders[j].volume >= market_sell:
+                    limit_order_book[i].orders[j].volume -= market_sell
+                else:
+                    market_sell -= limit_order_book[i].orders[j].volume
+                    limit_order_book[i].orders[j].volume = 0
+        for i in range(b, 100):
+            if i == b and limit_order_book[i].order_sum() == 0 and b != 100:
+                b += 1
+        for i in range(a, 0, -1):
+            if i == a and limit_order_book[i].order_sum() == 0 and a != 0:
+                a -= 1
+        t += DT
+        middle_prices[r].append(m)
+for i in range(10):
+    plt.plot([j for j in range(1000)], middle_prices[i])
+plt.show()
